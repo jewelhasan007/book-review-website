f6591fd8b568496b3caed3112ecfd1fd34c87551
Jewel Hasan <jewelhasan.eee@gmail.com>
2024-09-22 20:40:14 +0600

    login User info

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import app from "./firebase.init";
+import { useState } from "react";
 
 
 const LogIn = () => {
@@ -8,12 +9,14 @@ const LogIn = () => {
   console.log(app)
   const provider = new GoogleAuthProvider();
 
+  const [user, setUser] = useState([])
   const handleGoogleSignIn = ()=>{
 
     signInWithPopup(auth, provider)
     .then(result=>{
       const user = result.user;
       console.log(user);
+      setUser(user)
     })
     .catch(error=>{
       console.log('error', error.message)
@@ -23,6 +26,9 @@ const LogIn = () => {
     return (
         <div>
 <Helmet><title>Login || Book Vibe</title></Helmet>
+<h1>LogIn User Info:</h1>
+<h2>User Name:{user.displayName}</h2>
+<h3>Email: {user.email}</h3>
 <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
@@ -73,6 +79,7 @@ export default LogIn;
 // 8. Export default app: from the firebase.config.js file.
 // 9. login.jsx: import {getAuth} from 'firebase/auth'
 // 10. const auth =  getAuth(app); import app from /firebase.init
-// 11. const Provider = new GoogleAuthProvider();
+// 11. const Provider = new googleAuthProvider();
+// import googleAuthProvider and create a new provider.
 // 12. use signInWithPopUp and pass auth and provider.
 // 13. Activate sign-in method(for google, github etc) from console.firebase.google.com
