7e44ad8802b92b24fd972603dc878f0e3f2cf5f4
Jewel Hasan <jewel.hasan@cg-bd.com>
2024-09-25 17:02:01 +0600

    email added email

--- src/components/LogInEmail.jsx
+++ src/components/LogInEmail.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+
+
 const LogInEmail = () => {
+    const handleEmailLogIn = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+        // console.log('clicked')
+    }
     return (
         <div>
         <Helmet><title>LogInEmail || Book Vibe</title></Helmet>
-        
-        
-        
+
         <div className="hero bg-base-200 min-h-screen">
           <div className="hero-content flex-col">
                 <div className="text-center ">
                 <h1 className="text-5xl font-bold">Email Log In</h1>
               <p className="py-2 text-green-400">
-                If you are a user then Log in please.
+                Please Log in with Email.
               </p>
                 </div>
                
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-              <form className="card-body">
+              <form className="card-body" onSubmit={handleEmailLogIn}>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
