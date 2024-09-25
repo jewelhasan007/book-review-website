9816db45f6be8735da11705101de6666a20dc81e
Jewel Hasan <jewel.hasan@cg-bd.com>
2024-09-25 17:05:34 +0600

    email fixed

--- src/components/LogInEmail.jsx
+++ src/components/LogInEmail.jsx
@@ -30,13 +30,13 @@ const LogInEmail = () => {
                   <label className="label">
                     <span className="label-text">Email</span>
                   </label>
-                  <input type="email" placeholder="email" className="input input-bordered" required />
+                  <input name='email' type="email" placeholder="email" className="input input-bordered" required />
                 </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Password</span>
                   </label>
-                  <input type="password" placeholder="password" className="input input-bordered" required />
+                  <input name='password' type="password" placeholder="password" className="input input-bordered" required />
                   <label className="label">
                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                   </label>
