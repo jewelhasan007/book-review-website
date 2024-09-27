b3c9d06cba1420f75b15208e1a1a88e34f120d71
Jewel Hasan <jewelhasan.eee@gmail.com>
2024-09-28 01:06:42 +0600

    email login password

--- src/components/LogInEmail.jsx
+++ src/components/LogInEmail.jsx
@@ -19,15 +19,32 @@ const [passwordError, setPasswordError] = useState('');
         if(password.length < 4 ) {
           setPasswordError('the password is less than 4 characters');
           return
+      
         }
-        else if(!/A-Z/.test(password)){
-          setPasswordError('UPPERCASE MISSING')
+        else if(!/[A-Z]/.test(password)){
+          setPasswordError('UPPERCASE Missing')
+          return
         }
+        else if(!/[a-z]/.test(password)){
+          setPasswordError('lowercase Missing')
+          return
+        }
+        else if(!/[0-9]/.test(password)){
+          setPasswordError('number Missing')
+          return
+        }
+        else if(!/[!#$%&? "]/.test(password)){
+          setPasswordError('special character is missing')
+          return
+        }
+        setPasswordError('');
+        setUserSubmit('');
 const auth = getAuth(app)    
 createUserWithEmailAndPassword(auth, email, password)
 .then(result=>{
   const logInUser = result.user;
   setEmailUser(logInUser);
+  setUserSubmit(logInUser);
   console.log(emailUser);
 })
 .catch(error =>{
@@ -41,30 +58,35 @@ createUserWithEmailAndPassword(auth, email, password)
     return (
         <div>
         <Helmet><title>LogInEmail || Book Vibe</title></Helmet>
-
-     <div className='text-center'>
-     <div className={passwordError && passwordError ? 'text-red-500' : '' }>
-        User: {emailUser.displayName}
-      </div>
-      <div className={passwordError && passwordError ? 'text-red-500' : '' }>
-      Email: {emailUser.email}
-      </div>
-      <div className= { passwordError && passwordError ? 'text-red-500 font-bold' : ''}>
-      Email: { passwordError }
-      </div>
-     </div>
+    
+        <div className={passwordError && passwordError ? 'text-center hidden' : 'text-center' }>
+        <div className={userSubmit && !passwordError  ? 'text-green-500' : '' }>
+           User: {emailUser.displayName}
+         </div>
+         {
+   
+         }
+         <div className={userSubmit && !passwordError ? 'text-green-500' : '' }>
+         Email: {emailUser.email}
+         </div>
+         </div>
+  
 
         <div className="hero bg-base-200 min-h-screen">
           <div className="hero-content flex-col">
                 <div className="text-center ">
                 <h1 className="text-5xl font-bold">Email Log In</h1>
-              <p className="py-2 text-green-400">
-                Please Log in with Email.
-              </p>
+            
                 </div>
-               
+
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
               <form className="card-body" onSubmit={handleEmailLogIn}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input name='text' type="name" placeholder="name" className="input input-bordered" required />
+                </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
@@ -80,7 +102,21 @@ createUserWithEmailAndPassword(auth, email, password)
                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                   </label>
                 </div>
+                <div className="form-control flex flex-row">
+                
+                  <input className='mr-2' type="checkbox" name="terms" id="terms" />
+                  <label htmlFor="terms">I accept the <a href="" className='font-bold'>Terms & Conditions</a></label>
+                
+                </div>
                 <button className="btn btn-primary" >Submit</button>
+                <div className='mt-3'>
+                {
+                  userSubmit && userSubmit ? <span className='text-green-500 font-bold'>Successfully Submitted</span> : ''
+                }
+                </div>
+                <div className= { passwordError && passwordError ? 'text-red-500 font-bold' : ''}>
+                  { passwordError }
+                 </div>
               </form>
              
             </div>
